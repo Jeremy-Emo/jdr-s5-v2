88e4614538e07c3f80cd1551627333fed4913487
Jeremy-Emo <jeremy-emo@hotmail.Fr>
2020-11-21 15:39:45 +0100

    feat : filtre compétences

--- assets/js/skillList.js
+++ assets/js/skillList.js
@@ -17,10 +17,33 @@ $(document).ready(function () {
             }).fail(function (data) {
                 popErrorMessage(data.message);
             }).done(function (data) {
-                let levelBox = $this.closest('.skill_box').find('.level_box');
+                let skillBox = $this.closest('.skill_box')
+                let levelBox = skillBox.find('.level_box');
                 levelBox.text(parseInt(levelBox.text()) + 1);
-                skillPoints.text(parseInt(skillPoints.text()) - $this.data('skillcost'))
+                skillPoints.text(parseInt(skillPoints.text()) - $this.data('skillcost'));
+                if (!skillBox.hasClass("heroSkill")) {
+                    skillBox.addClass("heroSkill");
+                }
             });
+        }
+    });
+
+    body.on('click', '#toggleSkills', function () {
+        let $this = $(this);
+        if ($this.data('state') === "all") {
+            $this.text("Afficher toutes les compétences");
+            $('.skill_box').each(function(index) {
+                if (!$(this).hasClass('heroSkill')) {
+                    $(this).hide();
+                }
+            });
+            $this.data('state', 'my');
+        } else {
+            $this.text("Afficher mes compétences");
+            $('.skill_box').each(function(index) {
+                $(this).show();
+            });
+            $this.data('state', 'all');
         }
     });
 
